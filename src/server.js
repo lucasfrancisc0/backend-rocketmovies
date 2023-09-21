98cf1acd13d6723cc61108705e28595c69983b87
lucas <lucasluz1710@gmail.com>
2023-09-21 08:58:32 -0300

    Created error hadling

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const AppError = require("./utils/AppError");
 
 
 const app = express();
 app.use(express.json());
 
+app.use((error, request, response, next)  => {
+  if(error instanceof AppError) {
+    response.status(error.statuCode).json({
+      status: "Error.",
+      message: error.message
+    });
+  };
+
+  console.error(error);
+
+  response.status(500).json({
+    status: "Error.",
+    message: "Internal-Server-Error"
+  });
+});
 
 const PORT = 3333;
 app.listen(PORT, () => { console.log(`Server is running on port: ${PORT}`) });
---
+++ src/utils/AppError.js
@@ -0,0 +1,13 @@
+
+class AppError {
+  message;
+  statusCode;
+
+  constructor(message, statusCode) {
+    this.message = message;
+    this.statusCode = statusCode;
+  };
+};
+
+
+module.exports = AppError;
